53dfb2fa9e7f34572cc7b7a022d087a0e8db8228
Christian <christian@ambercreative.sg>
2020-08-25 11:40:53 +0800

    added logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,14 +15,14 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Your Wall',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/yourwall-logo.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,500&display=swap' }
     ]
   },
